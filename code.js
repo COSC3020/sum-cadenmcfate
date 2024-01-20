1a714662a8c3fb3dc8681474f3491d3f266ca2f3
cadenmcfate <113316495+cadenmcfate@users.noreply.github.com>
2024-01-20 11:44:03 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,6 @@
 function sum(a) {
-    if (a.length > 0) {
-        var sum = a[0];
-    } else {
-        var sum = 0; }
-    for(var i = 1; i < a.length; i++) {
+    var sum = 0;
+    for (var i = 1; i < a.length; i++) {
         sum += a[i];
-    }
     return sum;
 }
